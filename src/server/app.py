d55f6cea6969b7b12b67506414e9d6579e0b853e
John Lockwood <johnlockwood@me.com>
2021-12-09 23:19:44 +0000

    feat: Using migrations, added a completed column to the todo table

--- src/server/app.py
+++ src/server/app.py
@@ -17,11 +17,12 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id}, {self.desctription}>'
 
-db.create_all()
+# db.create_all()
 
 @app.route('/')
 def index():
